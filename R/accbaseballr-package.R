839b91ff0bea5519c40e8bcb1c5cb2a5404ecf56
Hana Baskin <hana.baskin@gmail.com>
2026-05-21 09:38:14 -0400

    Add package documentation

---
+++ R/accbaseballr-package.R
@@ -0,0 +1,8 @@
+#' accbaseballr: ACC Baseball Statistics and Metrics
+#'
+#' Provides ACC baseball batting and pitching datasets with
+#' advanced sabermetric metrics.
+#'
+#' @docType _PACKAGE
+#' @name accbaseballr
+NULL
